02a161faf82ed0aadc3bf8cfd2fb57aba61b3700
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-04 17:35:09 +0100

    ADD: compleatmento post ticket

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,31 @@
 // chiedere quanti chilometri si dovrà percorrere
 
-const numeroKm = parseFloat(prompt('inserisci i km da percorrere'));
+const numeroKm = parseFloat(prompt('inserisci i km da percorrere'));     //float
 
 // moltiplicare il numero dei km per 0.21€
 
-let costoIniziale = numeroKm * 0.21;
+let costoIniziale = numeroKm * 0.21;     //number
 
 // chiedere l'età del passeggiero
 
-const età = parseFloat(prompt('inserisci la tua età'));
+const eta = parseInt(prompt('inserisci la tua età'));     //number
 
-// applicare uno sconto del 20% o del 40% se nei parametri
+// calcolare lo sconto del 20% solo se minorenni o del 40% solo se over 65, altrimenti niente sconto
 
-if (età < 18){
-    console.log(costoIniziale * 0.20)
-} else if (età > 60){
-    console.log(costoIniziale * 0.40)
+let sconto;
+
+if (eta < 18){ // Se io sono minorenne
+    sconto = costoIniziale * 0.20; // Applico lo sconto del 20%
+} else if (eta > 65){ // Se io sono over 65
+    sconto = costoIniziale * 0.40; // Applico lo sconto del 40%
 } else {
-    console.log(costoIniziale * 1)
+    sconto = 0; // Applico lo sconto nullo
 }
 
+// calcolo prezzo finale biglietto
+
+const prezzoFinale = costoIniziale - sconto;     //number
+
 // dire il prezzo del biglietto arrotondato al centesimo
+
+console.log(prezzoFinale.toFixed(2));     //string
